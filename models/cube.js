7b7181902f9ecadc7a4a069f768122977a220926
AneliaPeycheva <anelyapeycheva@gmail.com>
2020-05-31 13:52:35 +0300

    feat:create cube model and save cube in the database

---
+++ models/cube.js
@@ -0,0 +1,42 @@
+
+const { v4 } = require('uuid');
+const fs = require('fs');
+const path = require('path')
+const databaseFile = path.join(__dirname, '..', 'config/database.json')
+
+class Cube {
+    constructor(name, description, imageUrl, difficulty) {
+        this.id = v4()
+        this.name = name || "No name"
+        this.description = description
+        this.imageUrl = imageUrl || "placeholder"
+        this.difficulty = difficulty || 1
+    }
+    save() {
+        const newCube = {
+            id: this.id,
+            name: this.name,
+            description: this.description,
+            imageUrl: this.imageUrl,
+            difficulty: this.difficulty
+        }
+        fs.readFile(databaseFile, (err, dbData) => {
+            if (err) {
+                throw err
+            }
+            const cubes= JSON.parse(dbData);
+            console.log(cubes);
+            cubes.push(newCube);
+
+            fs.writeFile(databaseFile, JSON.stringify(cubes), error => {
+                if (error) {
+                    throw error
+                }
+                console.log('new cube is succesfully stored')
+            })
+        })
+
+    }
+}
+
+module.exports = Cube
