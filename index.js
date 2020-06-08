984935c2f5fb4ae5e2afd154e8e289bc2ed67a7d
AneliaPeycheva <anelyapeycheva@gmail.com>
2020-06-08 12:12:16 +0300

    feat:connect with db

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const env = process.env.NODE_ENV || 'development';
+
+const mongoose=require('mongoose')
+const connectionStr = 'mongodb://localhost:27017/cubicledb'
 const config = require('./config/config')[env];
 const express=require('express')
 const indexRouter=require('./routes')
 const app = express();
+mongoose.connect(connectionStr,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+},function (err) {
+    if (err) {
+        console.error('Something happend with DB')
+        throw err
+    }
+    console.log('Database is setup and running')
+})
 
-require('./config/express')(app);
+require('./config/express')(app)
 app.use('/',indexRouter)
+
 
 app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
