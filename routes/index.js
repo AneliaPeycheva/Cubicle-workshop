e8de7343df17017ff4ed531162b8a939b266fbeb
AneliaPeycheva <anelyapeycheva@gmail.com>
2020-06-13 11:05:56 +0300

    feat:add new accessories pages

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,18 @@
 const { Router } = require('express');
-const { getAllCubes,getCube } = require('../controllers/cubes')
-
-const Cube=require('../models/cube')
+const { getAllCubes, getCube, updateCube, getCubeWithAccessories } = require('../controllers/cubes')
+const { getAccessories } = require('../controllers/accessories')
+const Cube = require('../models/cube')
+const Accessory = require('../models/accessory')
 const router = Router()
 
-router.get('/', async (req, res) => { 
-    const cubes=await getAllCubes()
-  
-        res.render('index', {
-            title: 'Cube workshop',
-            cubes:cubes
-        })
+router.get('/', async (req, res) => {
+    const cubes = await getAllCubes()
+
+    res.render('index', {
+        title: 'Cube workshop',
+        cubes: cubes
     })
+})
 
 
 
@@ -28,32 +29,82 @@ router.get('/create', (req, res) => {
 })
 
 router.post('/create', (req, res) => {
-   const {
-       name,
-       description,
-       imageUrl,
-       difficultyLevel
-    }=req.body
+    const {
+        name,
+        description,
+        imageUrl,
+        difficultyLevel
+    } = req.body
 
-    const cube=new Cube({name,description,imageUrl,difficultyLevel})
+    const cube = new Cube({ name, description, imageUrl, difficultyLevel })
 
-    cube.save((err)=>{
-        if(err){
-            console.log(err)
+    cube.save((err) => {
+        if (err) {
+            console.error(err)
+            res.redirect('/create')
         } else {
             res.redirect('/')
-        }       
+        }
     })
 })
 
 router.get('/details/:id', async (req, res) => {
-    const cube=await getCube(req.params.id)
-         res.render('details', {
+    const cube = await getCubeWithAccessories(req.params.id)
+    res.render('details', {
         title: 'Details | Cube Workshop',
-        ...cube  
+        ...cube
     })
-  })
+})
 
+router.get('/create/accessory', (req, res) => {
+    res.render('createAccessory', {
+        title: 'Create accessory'
+    })
+})
+
+router.post('/create/accessory', async (req, res) => {
+    const {
+        name,
+        description,
+        imageUrl
+    } = req.body
+
+    const accessory = new Accessory({ name, description, imageUrl })
+
+    await accessory.save((err) => {
+        if (err) {
+            console.error(err)
+            res.redirect('/create/accessory')
+        } else {
+            res.redirect('/')
+        }
+    })
+})
+
+router.get('/attach/accessory/:id', async (req, res) => {
+    const cube = await getCube(req.params.id)
+    let accessories = await getAccessories()
+    
+    const canAttachAccessory = cube.accessories.length !== accessories.length && accessories.length > 0
+    res.render('attachAccessory', {
+        title: 'Attach accessory',
+        ...cube,
+        accessories,
+        canAttachAccessory
+    })
+})
+
+router.post('/attach/accessory/:id', async (req, res) => {
+    console.log(req)
+    const {
+        accessory
+    } = req.body
+    
+    console.log(req.params.id)
+    await updateCube(req.params.id, accessory)
+
+    res.redirect(`/details/${req.params.id}`)
+})
 
 router.get('*', (req, res) => {
     res.render('404', {
